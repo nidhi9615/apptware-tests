f38efa17150db0a297a00ae5fe233eb7d735a689
nidhi9615 <nidhikushwaha9615@gmail.com>
2024-05-03 13:58:08 +0530

    Update Student.java
    added some changes

--- src/main/java/com/apptware/interview/immutability/Student.java
+++ src/main/java/com/apptware/interview/immutability/Student.java
@@ -1,15 +1,22 @@
+
 /** This class is expected to be immutable. Please make necessary changes. */
 package com.apptware.interview.immutability;
 
 import java.util.Date;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class Student {
-  private String name;
-  private Date dateOfBirth;
-  private List<String> courses;
+public final class Student {
+  private final String name;
+  private final Date dateOfBirth;
+  private final List<String> courses;
+  
+  
+public Student(String name, Date dateOfBirth, List<String> courses) {
+	super();
+	this.name = name;
+	this.dateOfBirth = dateOfBirth;
+	this.courses = courses;
+}
+  
+  
 }
